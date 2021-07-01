5e7e73cef42941a11b84ae4fb7d22721443f8fb7
Bepen Neupane <bepen3@gmail.com>
2021-07-01 18:54:08 -0400

    feat: placeholder selfie with description

--- client/src/Home/Home.tsx
+++ client/src/Home/Home.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import './Home.css';
+import selfie from '../images/placeholder.jpg'
 
-function Home() {
-  return (
-    <div className="Home">
-      Hello, world!
-    </div>
-  );
+export default class Home extends React.Component {
+
+  firstName: string = "Bepen";
+  lastName: string = "Neupane";
+
+
+  getFullNameGreeting(): JSX.Element {
+    const fullName = `${this.firstName} ${this.lastName}`
+    return (
+      <div className="Home">
+        Hello, world! My name is {fullName}.
+      </div>
+    );
+  }
+
+  getImageElement(): JSX.Element {
+    return <img src={selfie} alt="Selfie" />;
+  }
+
+  render() {
+    const label: JSX.Element = this.getFullNameGreeting();
+    const photo: JSX.Element = this.getImageElement();
+    return (
+      <React.Fragment>
+        {photo}
+        {label}
+      </React.Fragment>
+    );
+  }
 }
-
-export default Home;
